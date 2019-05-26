4356b0f4944b97e7baabc2b0fff88263b04b8c73
Stefan Rimaila <stefan@rimaila.fi>
2019-05-26 21:39:46 +0300

    Add options, improve stdout

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 #!/usr/bin/env node
 require('dotenv').config();
 
+const output = require('./lib/output');
+
 const argv =
   require('yargs')
     .version()
     .env('ETOMUSUME')
     .option('verbose', {
       alias: 'v',
+      describe: 'Would you like it noisy, Sir?'
     })
     .option('video-id', {
       alias: 'i',
+      describe: 'Source video ID',
     })
     .option('twitch-client-id', {
-      alias: 'c',
+      describe: 'Twitch API client ID',
     })
     .option('destination', {
       alias: 'd',
       default: 'stdout',
+      choices: Object.keys(output),
+      describe: 'Where to redirect output to. Defaults to `stdout`',
+    })
+    .option('print0', {
+      alias: '0',
+      describe: 'Use null character as delimiter for items',
     })
     .demandOption([
       'video-id',
